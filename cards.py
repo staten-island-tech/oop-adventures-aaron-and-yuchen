abc1fa6fbe25abf92a8fd7ed48fc937d348db67d
gryuewua <yuchenw7@nycstudents.net>
2026-05-06 09:23:06 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,13 +1,28 @@
 import requests
 
 def draw(amount):
+        present = False
         cards = {}
+        aces = []
         cardvalue = 0
-        response = requests.get(f"https://deckofcardsapi.com/api/deck/new/draw/?count={amount}")
+        response = requests.get(f"https://deckofcardsapi.com/api/deck/1nze49wxn3h1/draw/?count={amount}")
         if response.status_code != 200:
             print("Error fetching data!")
             return None
         cards_drawn = response.json()
+        print(cards_drawn["remaining"])
+        if amount > cards_drawn["remaining"] and cards_drawn["remaining"] != 0:
+            print("Not enough cards, enter a valid value.")
+            amount = 0
+        elif cards_drawn['remaining'] == 0:
+            print("You are out of cards...")
+            response = requests.get("https://deckofcardsapi.com/api/deck/1nze49wxn3h1/shuffle/")
+            print("The deck has been shuffled!")
+            return None
+        if int(cards_drawn["remaining"]) <= 0:
+            print("You are out of cards...")
+            response = requests.get("https://deckofcardsapi.com/api/deck/1nze49wxn3h1/shuffle/")
+            print("The deck has been shuffled!")
         for x in range(amount):
             if cards_drawn['cards'][x]['value'] == 'QUEEN' or cards_drawn['cards'][x]['value'] == 'KING' or cards_drawn['cards'][x]['value'] == 'JACK':
                 cards[x] = {
@@ -18,8 +33,7 @@ def draw(amount):
             elif cards_drawn['cards'][x]['value'] == 'ACE':
                 cards[x] = {
                     'code': cards_drawn['cards'][x]['code'],
-                    'value_1': 1,
-                    'value_11': 11,
+                    'value': 11,
                     'suit': cards_drawn['cards'][x]['suit']
                 }
             else:
@@ -31,9 +45,15 @@ def draw(amount):
         for y in cards:
             print(cards[y])
             cardvalue += int(cards[y]['value'])
-        if cardvalue > 21:
-            print("YOU SUCK")
-        else:
-            print(cardvalue)
-
-draw(2)
+            if cards[y]['value'] == 11:
+                aces.append(1)
+                present = True
+            if cardvalue > 21:
+                if len(aces) > 0 and present == True:
+                    cardvalue -= 10
+                    present = False
+                print(cardvalue)
+            else:
+                print(cardvalue)
+                 
+draw(3)
