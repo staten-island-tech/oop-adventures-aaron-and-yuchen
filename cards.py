f0279b7cd8427fae1da4fc408aafcdf37fef86ed
gryuewua <yuchenw7@nycstudents.net>
2026-05-06 09:14:44 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,14 +1,28 @@
 import requests
 
 def draw(amount):
+        present = False
         cards = {}
         aces = []
         cardvalue = 0
-        response = requests.get(f"https://deckofcardsapi.com/api/deck/new/draw/?count={amount}")
+        response = requests.get(f"https://deckofcardsapi.com/api/deck/1nze49wxn3h1/draw/?count={amount}")
         if response.status_code != 200:
             print("Error fetching data!")
             return None
         cards_drawn = response.json()
+        print(cards_drawn["remaining"])
+        if amount > cards_drawn["remaining"] and cards_drawn["remaining"] != 0:
+            print("Not enough cards, enter a valid value.")
+            amount = 0
+        elif cards_drawn['remaining'] == 0:
+            print("You are out of cards...")
+            response = requests.get("https://deckofcardsapi.com/api/deck/1nze49wxn3h1/shuffle/")
+            print("The deck has been shuffled!")
+            return None
+        if int(cards_drawn["remaining"]) <= 0:
+            print("You are out of cards...")
+            response = requests.get("https://deckofcardsapi.com/api/deck/1nze49wxn3h1/shuffle/")
+            print("The deck has been shuffled!")
         for x in range(amount):
             if cards_drawn['cards'][x]['value'] == 'QUEEN' or cards_drawn['cards'][x]['value'] == 'KING' or cards_drawn['cards'][x]['value'] == 'JACK':
                 cards[x] = {
@@ -33,12 +47,13 @@ def draw(amount):
             cardvalue += int(cards[y]['value'])
             if cards[y]['value'] == 11:
                 aces.append(1)
+                present = True
             if cardvalue > 21:
-                if len(aces) > 0:
+                if len(aces) > 0 and present == True:
                     cardvalue -= 10
+                    present = False
                 print(cardvalue)
             else:
-                print(cardvalue)    
-            
+                print(cardvalue)
                  
-draw(3)
+draw(2)
