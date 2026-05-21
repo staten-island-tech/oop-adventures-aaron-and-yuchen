6b0e46f320033b929bdbec3b70a14f28b323b5dd
aaronw176 <aaronw176@nycstudents.net>
2026-05-21 09:15:36 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -371,6 +371,7 @@ money = 100
 balance = pboy.__dict__
 stay = "yes"
 leave = False
+print("So all the number value is their number, 0, J-K is 10 and A is 11 (1 if go over 21).")
 print(f"You start with ${money}")
 while money > 0 and leave == False:
     if stay == "yes":
